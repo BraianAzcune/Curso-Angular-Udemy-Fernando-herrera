ad7691bde3c78db268147a9db5fc04e4f1b3c96f
BraianAzcune <braian.s.azcune@hotmail.com>
2023-03-10 15:44:26 -0300

    mostrar varios mapas en fila

--- 21-mapasApp/src/app/mapas/components/mini-mapa/mini-mapa.component.ts
+++ 21-mapasApp/src/app/mapas/components/mini-mapa/mini-mapa.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, ViewChild} from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-mini-mapa',
   templateUrl: './mini-mapa.component.html',
   styleUrls: ['./mini-mapa.component.css']
 })
-export class MiniMapaComponent implements OnInit {
+export class MiniMapaComponent implements AfterViewInit {
 
-  constructor() { }
+  @Input() lngLat: [number,number]= [0,0];
 
-  ngOnInit(): void {
+  @ViewChild('mapa') divMapa!: ElementRef;
+
+  mapa!: mapboxgl.Map;
+  ngAfterViewInit(): void {
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat, // starting position [lng, lat]
+      zoom: 16, // starting zoom
+      interactive: false,
+    });
+    new mapboxgl.Marker().setLngLat(this.lngLat).addTo(this.mapa);
   }
-
 }
