0aa3d724c3d40067182a43a71a4b1fdac81552fe
BraianAzcune <braian.s.azcune@hotmail.com>
2023-05-21 23:52:45 -0300

    remover .git dentro del proyecto

---
+++ 23-DirectivasYSignal/src/app/products/pages/product-page/product-page.component.ts
@@ -0,0 +1,28 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+@Component({
+  templateUrl: './product-page.component.html',
+  styleUrls: ['./product-page.component.css']
+})
+export class ProductPageComponent {
+
+  // constructor(private fb: FormBuilder){}
+  private fb = inject(FormBuilder);
+
+  public formulario: FormGroup = this.fb.group({
+    nombre: ["", [Validators.required, Validators.minLength(6), Validators.email]]
+  });
+
+  public color: string = "green";
+
+  changeColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    this.color = color;
+  }
+}
