24fafe6f874b994980418c3e9ae8c6d58bd1c0c0
BraianAzcune <braian.s.azcune@hotmail.com>
2023-05-28 18:27:46 -0300

    se conecto nest a mongo, y se creo schema de usuario

---
+++ 25-Auth-MEAN-nest-angular/nest-backend/src/auth/entities/user.entity.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema()
+export class User {
+  // mongo crea el _id
+  @Prop({ default: false })
+  isActive: boolean;
+  @Prop({ unique: true, required: true })
+  email: string;
+  @Prop({ required: true, minlength: 6 })
+  password: string;
+  @Prop({ required: true })
+  name: string;
+  // redundancia, pero bueno...
+  @Prop({ type: [String], default: ['user'] })
+  roles: string[];
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
